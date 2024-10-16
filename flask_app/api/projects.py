2bab172920c1bbe092c5c05ee4a35bb104ae6a57
Nas <jonas.reblora.01@gmail.com>
2024-10-16 14:07:25 +0800

    Updates

---
+++ flask_app/api/projects.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, request, jsonify
+
+project_bp = Blueprint('projects', __name__)
+
+@project_bp.route('/project', methods=['GET'])
+def get_project():
+    projects = [
+        {"name": "Project 1", "Portfolio": "My first project"}
+    ]
+    return jsonify(projects)
