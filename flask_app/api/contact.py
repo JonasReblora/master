7478327eeb26c63f281b1d5fdd835328b0edf655
Nas <jonas.reblora.01@gmail.com>
2024-10-16 23:39:27 +0800

    Updates

--- flask_app/api/contact.py
+++ flask_app/api/contact.py
@@ -4,10 +4,12 @@ contact_bp = Blueprint('contact', __name__)
 
 @contact_bp.route('/', methods=["POST"])
 def contact():
-    data = request.form.get_json()
     name = request.form.get('name')
     email = request.form.get('email')
     msg = request.form.get('msg')
+
+    if not name or not email or not msg:
+        return jsonify({"status": "error", "message": "Missing form data"}), 400
 
     return jsonify({
         "status": "success",
