08bf579d98d976772657cc061dbe6a532a5e0a2a
Nas <jonas.reblora.01@gmail.com>
2024-10-16 13:59:28 +0800

    Updates

---
+++ flask_app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from api.projects import projects_bp
+from api.contact import contact_bp
+
+app = Flask(__name__)
+
+# Register blueprints
+app.register_blueprint(projects_bp)
+app.register_blueprint(contact_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
